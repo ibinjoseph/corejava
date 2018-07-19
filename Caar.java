9c7bec7dacdfbca70849cc1a0674b2ab4249bd8f
ibinjoseph <41329836+ibinjoseph@users.noreply.github.com>
2018-07-19 11:42:40 +0530

    Add files via upload

---
+++ Caar.java
@@ -0,0 +1,48 @@
+public class Caar {
+ private String carName;
+ private double mileage, price;
+ private boolean luxuryType;
+ 
+ public void setCarDetails(String carName, double mileage, double price, boolean luxuryType) {
+  this.carName = carName;
+  this.mileage = mileage;
+  this.price = price;
+  this.luxuryType = luxuryType;
+ }
+ @Override
+ public String toString() {
+  return "Car [carName=" + carName + ", mileage=" + mileage + ", price=" + price + ", luxuryType=" + luxuryType
+    + "]";
+ }
+ 
+ public String getCarName() {
+  return carName;
+ }
+ public double getMileage() {
+  return mileage;
+ }
+ public void setMileage(double mileage) {
+  this.mileage = mileage;
+ }
+ public boolean isLuxuryType() {
+  return luxuryType;
+ }
+ public void setLuxuryType(boolean luxuryType) {
+  this.luxuryType = luxuryType;
+ }
+ public void setPrice(double price) {
+  this.price = price;
+ }
+ public void setCarName(String carName) {
+  this.carName = carName;
+ }
+ public double getPrice() {
+  return price;
+ }
+ public void printCarDetails() {
+  this.toString();
+ }
+ 
+ 
+ 
+}
